96b2bc77ff0fcd8063e167f9b361a778cec08dd4
Ben <benawhiting@gmail.com>
2024-05-23 17:00:59 -0700

    Update to use csv lib

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,1 +1,14 @@
-puts 'Event Manager Initialized!'
+require 'csv'
+puts 'EventManager initialized.'
+
+contents = CSV.open(
+  'event_attendees.csv',
+  headers: true,
+  header_converters: :symbol
+)
+
+contents.each do |row|
+  name = row[:first_name]
+  zipcode = row[:zipcode]
+  puts "#{name} #{zipcode}"
+end
