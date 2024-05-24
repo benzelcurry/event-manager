4e0e27e28633c8e2aca7da3f6b15be5797c4fb46
Ben <benawhiting@gmail.com>
2024-05-24 15:53:43 -0700

    Clean up zipcode cleaning method

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -8,15 +8,7 @@ contents = CSV.open(
 )
 
 def clean_zipcode(zipcode)
-  if zipcode.nil?
-    '00000'
-  elsif zipcode.length < 5
-    zipcode.rjust(5, '0')
-  elsif zipcode.length > 5
-    zipcode[0..4]
-  else
-    zipcode
-  end
+  zipcode.to_s.rjust(5, '0')[0..4]
 end
 
 contents.each do |row|
