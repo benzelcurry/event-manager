cd84baab5bbcdcbd02b58f1923b07e4265cda52a
Ben <benawhiting@gmail.com>
2024-05-31 15:04:22 -0700

    Upgrade program to utilize erb

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,7 +1,9 @@
 require 'csv'
 require 'google/apis/civicinfo_v2'
+require 'erb'
 
-template_letter = File.read('form_letter.html')
+template_letter = File.read('form_letter.erb')
+erb_template = ERB.new template_letter
 
 contents = CSV.open(
   'event_attendees.csv',
@@ -15,22 +17,20 @@ end
 
 def legislators_by_zipcode(zip)
   civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
-  # Okay to leave the below key publicly exposed; came from a public source
+  # Okay to leave the below key exposed; is public
   civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
 
   begin
-    legislators = civic_info.representative_info_by_address(
+    civic_info.representative_info_by_address(
       address: zip,
       levels: 'country',
       roles: ['legislatorUpperBody', 'legislatorLowerBody']
-    )
-    legislators = legislators.officials
-    legislator_names = legislators.map(&:name)
-    legislator_names.join(', ')
+    ).officials
   rescue
     'You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials'
   end
 end
+
 
 puts 'EventManager initialized.'
 
@@ -38,9 +38,7 @@ contents.each do |row|
   name = row[:first_name]
   zipcode = clean_zipcode(row[:zipcode])
   legislators = legislators_by_zipcode(zipcode)
-
-  personal_letter = template_letter.gsub('FIRST_NAME', name)
-  personal_letter.gsub!('LEGISLATORS', legislators)
+  personal_letter = erb_template.result(binding)
 
   puts personal_letter
 end
